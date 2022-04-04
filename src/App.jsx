c4b7bc26e567a7da23e7e15b6d65d6eb01bf7904
Geovanne <callegarog00@gmail.com>
2022-04-04 13:46:34 -0300

    featured movies component made for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,11 @@ export default function App() {
       setMovieList(list)
 
       //Pegando o featured 
-      let originals = list.filter(item => item.slug === 'originals')
+      let originals = list.filter(i => i.slug === 'originals')
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
     }
 
     loadAll()
